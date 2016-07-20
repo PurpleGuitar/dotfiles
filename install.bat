106590e1328f3309bcae0e00402afeb92367db92
Craig Oliver <craig.robert.oliver@gmail.com>
2016-07-20 10:31:48 -0400

    Customize install.bat

--- install.bat
+++ install.bat
@@ -8,14 +8,37 @@ if NOT %errorLevel% == 0 (
     goto end
 )
 
+rem remember current dir
+set OLDDIR=%CD%
+
 rem Get script dir
 set SCRIPT_DIR=%~dp0
 
 rem Get target dir
 set TARGET_DIR=%USERPROFILE%
-cd %TARGET_DIR%
+
+rem Process parameters
+set SETUP_VIM=true
+rem Modified from: http://stackoverflow.com/questions/14286457/using-parameters-in-batch-files-at-dos-command-line
+:parse
+IF "%~1"=="" GOTO endparse
+IF "%~1"=="--help" (
+    echo Usage: install.bat
+    echo Options:
+    echo   --help: Print this help and exit
+    echo   --no-vim: Don't setup vim
+    goto end
+)
+IF "%~1"=="--no-vim" (
+    set SETUP_VIM=false
+)
+SHIFT
+GOTO parse
+:endparse
+
 
 rem Link to dotfiles
+cd %TARGET_DIR%
 for %%x in (
     vimrc
     vimrc-plugins
@@ -28,16 +51,22 @@ for %%x in (
 )
 
 
-rem Create new .vim directory
-rmdir /s /q .vim
-mkdir .vim
+if "%SETUP_VIM%"=="true" (
+    cd %TARGET_DIR%
+    rem Create new .vim directory
+    rmdir /s /q .vim
+    mkdir .vim
 
-rem Install plugins
-mkdir .vim\bundle
-cd .vim\bundle
-git clone https://github.com/VundleVim/Vundle.vim.git
-git clone https://github.com/PurpleGuitar/vim-croz-colorscheme.git
-cd %TARGET_DIR%
-vim "+PluginInstall" "+qall"
+    rem Install plugins
+    mkdir .vim\bundle
+    cd .vim\bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git
+    git clone https://github.com/PurpleGuitar/vim-croz-colorscheme.git
+    cd %TARGET_DIR%
+    vim "+PluginInstall" "+qall"
+)
 
 :end
+
+rem Restore old current directory
+chdir /d %OLDDIR%
