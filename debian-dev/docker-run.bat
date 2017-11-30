05bed85964e5bf5bdc7eaeecfd5ff4b3f7d1a4ce
Craig Oliver <craig.robert.oliver@gmail.com>
2017-11-30 10:00:55 -0500

    Optionally share dotfiles with host

--- debian-dev/docker-run.bat
+++ debian-dev/docker-run.bat
@@ -1,5 +1,17 @@
 @echo off
 
+rem If you'd like to share dotfiles between the host and client, then set
+rem the environment variable DOTFILES_LOC to point to the host directory
+rem where they live.
+rem 
+rem Or if you'd prefer the client keeps its own private copy of the
+rem dotfiles, then create a volume and set DOTFILES_LOC to the name of the
+rem volume.
+rem
+rem If you'd prefer not to set an environment variable at all, you can
+rem uncomment the next line and set it in this script.
+rem DOTFILES_LOC=docker-dotfiles-volume
+
 rem get IP of Windows host
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set HostIP=%%a
 
@@ -10,7 +22,7 @@ docker run ^
        --tty ^
        --hostname debian-dev ^
        --env DISPLAY=%HostIP%:0 ^
-       --volume debian-dev-dotfiles:/root/dotfiles ^
+       --volume %DOTFILES_LOC%:/root/dotfiles ^
        --volume C:/:/mnt/host ^
        --volume %USERPROFILE%:/root/host-home ^
        debian-dev ^
